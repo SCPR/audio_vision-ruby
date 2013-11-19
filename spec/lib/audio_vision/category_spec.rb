fec6cc556745cced9a5acc70b3b473b8da468a63
Bryan Ricker <bricker88@gmail.com>
2013-11-19 13:40:29 -0800

    Fix UTF-8 error

--- spec/lib/audio_vision/category_spec.rb
+++ spec/lib/audio_vision/category_spec.rb
@@ -1,3 +1,4 @@
+# UTF-8
 require 'spec_helper'
 
 describe AudioVision::Category do
@@ -7,7 +8,7 @@ describe AudioVision::Category do
         "id" => 2,
         "title" => "Images",
         "slug" => "images",
-        "description" => "The heartbeat of AudioVision. The best from our team of photojournalists — and discover images from the photographers who inspire them.",
+        "description" => "The heartbeat of AudioVision. The best from our team of photojournalists - and discover images from the photographers who inspire them.",
         "public_url" => "http://audiovision.scpr.org/images",
         "permalink" => "http://audiovision.scpr.org/images"
       }
@@ -16,7 +17,7 @@ describe AudioVision::Category do
     category.id.should eq 2
     category.title.should eq "Images"
     category.slug.should eq "images"
-    category.description.should eq "The heartbeat of AudioVision. The best from our team of photojournalists — and discover images from the photographers who inspire them."
+    category.description.should eq "The heartbeat of AudioVision. The best from our team of photojournalists - and discover images from the photographers who inspire them."
     category.public_url.should eq "http://audiovision.scpr.org/images"
   end
 end
